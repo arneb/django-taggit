arneb/django-taggit

commit 3de3136ff875e0a50ff293f7ca6f84e4f1646684

don't remove values from the input field

taggit/widgets.py
@@ -17,31 +17,32 @@ class TagAutocomplete(forms.TextInput):
             attrs = dict(self.attrs.items() + attrs.items())
         list_view = reverse('taggit-list')
         if value is not None and not isinstance(value, basestring):
-            value = simplejson.dumps(
-              [{'value': u'%s' % edit_string_for_tags([o.tag])} for o in value.select_related("tag")]
-            )
+            value = [edit_string_for_tags([o.tag]) for o in value.select_related("tag")]
         else:
-            value = []
+            value = value.split(',')
         html = super(TagAutocomplete, self).render(
             name+"_dummy",
-            u'',
+            ",".join(value),
             attrs
         )
         allow_add = "false"
         if 'allow_add' in attrs and attrs['allow_add']:
             allow_add = "true"
-        js_config = u"{startText: \"%s\", \
-            preFill: %s, \
+        js_config = u"""{startText: "%s", \
+            preFill: prefill, \
             allowAdd: %s, \
-            allowAddMessage: \"%s\"}" % (
+            allowAddMessage: "%s"}""" % (
                 escapejs(_("Enter Tag Here")),
-                value,
                 allow_add,
                 escapejs(_("Please choose an existing tag")),
             )
-        js = u"<script type=\"text/javascript\">jQuery = django.jQuery; \
-            jQuery().ready(function() { jQuery(\"#%s\").autoSuggest(\"%s\", \
-            %s); });</script>" % (
+        js = u"""<script type="text/javascript">jQuery = django.jQuery; \
+            jQuery().ready(function() { \
+            var prefill = [];
+            jQuery.each(jQuery('input[name="%s_dummy"]').val().split(','),function(i,v){prefill.push({'value': v})});
+            jQuery("#%s").autoSuggest("%s", \
+            %s); });</script>""" % (
+                name,
                 attrs['id'],
                 list_view,
                 js_config

commit 237d308a8ccf7400e20e76725837b2cdad004237

fix validating empty fields by providing a _has_changed method (bug was probably introduced in 3de3136)

taggit/widgets.py
@@ -6,7 +6,7 @@ from django.utils.html import escapejs
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
-from taggit.utils import edit_string_for_tags
+from taggit.utils import edit_string_for_tags, parse_tags
 
 
 class TagAutocomplete(forms.TextInput):
@@ -50,14 +50,35 @@ class TagAutocomplete(forms.TextInput):
             var prefill = [];
             jQuery.each(jQuery('input[name="%s_dummy"]').val().split(','),function(i,v){prefill.push({'value': v})});
             jQuery("#%s").autoSuggest("%s", \
-            %s); });</script>""" % (
+            %s);});</script>""" % (
                 name,
                 attrs['id'],
                 list_view,
                 js_config
             )
         return mark_safe("\n".join([html, js]))
-    
+
+    def _has_changed(self, initial, data):
+        """
+        Called by BaseForm._get_changed_data, which sends this the form's initial value
+        for the field and the raw value that was submitted to the form, *before it has
+        been through any clean() methods.*
+
+        This means that the initial data will (usually) be a related Tag manager, while
+        the raw data will (usually) be a string. So, they're both converted to strings
+        before sending them to the regular change comparison.
+        """
+        if initial is not None and not isinstance(initial, basestring):
+            initial = edit_string_for_tags([o.tag for o in initial.select_related("tag")])
+
+        if data is not None and not isinstance(data, basestring):
+            data = edit_string_for_tags([o.tag for o in data.select_related("tag")])
+        
+        if data is not None and isinstance(data, basestring):
+            data = edit_string_for_tags(parse_tags(data))
+            
+        return super(TagAutocomplete, self)._has_changed(initial, data)
+
     class Media:
         js_base_url = getattr(
             settings,
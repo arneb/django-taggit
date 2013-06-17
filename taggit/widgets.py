237d308a8ccf7400e20e76725837b2cdad004237
Arne Brodowski <arne@rcs4u.de>
2013-06-17 12:59:15 +0200

    fix validating empty fields by providing a _has_changed method (bug was probably introduced in 3de3136)

--- taggit/widgets.py
+++ taggit/widgets.py
@@ -6,7 +6,7 @@ from django.utils.html import escapejs
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
-from taggit.utils import edit_string_for_tags
+from taggit.utils import edit_string_for_tags, parse_tags
 
 
 class TagAutocomplete(forms.TextInput):
@@ -50,14 +50,35 @@ class TagAutocomplete(forms.TextInput):
             var prefill = [];
             jQuery.each(jQuery('input[name="%s_dummy"]').val().split(','),function(i,v){prefill.push({'value': v})});
             jQuery("#%s").autoSuggest("%s", \
-            %s); });</script>""" % (
+            %s);});</script>""" % (
                 name,
                 attrs['id'],
                 list_view,
                 js_config
             )
         return mark_safe("\n".join([html, js]))
-    
+
+    def _has_changed(self, initial, data):
+        """
+        Called by BaseForm._get_changed_data, which sends this the form's initial value
+        for the field and the raw value that was submitted to the form, *before it has
+        been through any clean() methods.*
+
+        This means that the initial data will (usually) be a related Tag manager, while
+        the raw data will (usually) be a string. So, they're both converted to strings
+        before sending them to the regular change comparison.
+        """
+        if initial is not None and not isinstance(initial, basestring):
+            initial = edit_string_for_tags([o.tag for o in initial.select_related("tag")])
+
+        if data is not None and not isinstance(data, basestring):
+            data = edit_string_for_tags([o.tag for o in data.select_related("tag")])
+        
+        if data is not None and isinstance(data, basestring):
+            data = edit_string_for_tags(parse_tags(data))
+            
+        return super(TagAutocomplete, self)._has_changed(initial, data)
+
     class Media:
         js_base_url = getattr(
             settings,
